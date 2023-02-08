64b8825938d5553364478a72140c0852e5719f20
kevin r minn <52860121+kevinrjminn@users.noreply.github.com>
2023-02-08 18:32:27 -0500

    a0

---
+++ Assignment0/CIS_457/html/images/grjava3e/Fr3DHDashed.java
@@ -0,0 +1,37 @@
+// Fr3DHDashed.java: Frame class for HLinesDashed.java.
+// This class extends Fr3D to enable writing HP-GL output files.
+
+import java.awt.*;
+import java.awt.event.*;
+
+class Fr3DHDashed extends Fr3D {
+   private MenuItem exportHPGL;
+   CvHLinesDashed cv;
+
+   Fr3DHDashed(String argFileName, CvHLinesDashed cv, String textTitle) {
+      super(argFileName, cv, textTitle);
+      exportHPGL = new MenuItem("Export HP-GL");
+      mF.add(exportHPGL); 
+      exportHPGL.addActionListener(this);
+      this.cv = cv;
+   }
+
+   public void actionPerformed(ActionEvent ae) {
+      if (ae.getSource() instanceof MenuItem) {
+         MenuItem mi = (MenuItem)ae.getSource();
+         if (mi == exportHPGL) {
+            Obj3D obj = cv.getObj();
+            if (obj != null) {
+               cv.setHPGL(new HPGL(obj));
+               cv.repaint();
+            }
+            else 
+               Toolkit.getDefaultToolkit().beep();
+         } 
+         else
+            super.actionPerformed(ae);
+      }
+   }
+}
+
+
